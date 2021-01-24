5965ccedb2418b12bde24c0f4584baa84e9dd577
MiguelNdeCarvalho <geral@miguelndecarvalho.pt>
2021-01-24 18:31:04 +0000

    MvcConfig: Add ViewController for Registar

--- pingu-shop/src/main/java/org/pingu/shop/jpa/MvcConfig.java
+++ pingu-shop/src/main/java/org/pingu/shop/jpa/MvcConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class MvcConfig implements WebMvcConfigurer {
 	
 	public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/register").setViewName("register");
+        registry.addViewController("/registar").setViewName("registar");
         registry.addViewController("/login").setViewName("login");
 	}
 }
