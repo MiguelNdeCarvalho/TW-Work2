6b9aa74057c7b6759ea80e0fb6199ed4b94ea350
MiguelNdeCarvalho <geral@miguelndecarvalho.pt>
2021-01-24 17:26:34 +0000

    Add UserDetailsServiceImpl class

--- pingu-shop/src/main/java/org/pingu/shop/jpa/UserRepository.java
+++ pingu-shop/src/main/java/org/pingu/shop/jpa/UserRepository.java
@@ -9,4 +9,5 @@ public interface UserRepository extends CrudRepository<User, Long> {
 	List<User> findByFirstName(String firstName);
 
 	User findById(long id);
+	User findOneByUsername(String username);
 }
