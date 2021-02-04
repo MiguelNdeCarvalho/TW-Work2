81d2d292e279e838a01693a39b3f945da0118bc5
MiguelNdeCarvalho <geral@miguelndecarvalho.pt>
2021-02-04 15:49:32 +0000

    Add ProductRepository.java

---
+++ pingu-shop/src/main/java/org/pingu/shop/jpa/ProductRepository.java
@@ -0,0 +1,9 @@
+package org.pingu.shop.jpa;
+
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ProductRepository extends CrudRepository<Product, Long> {
+    Product findByName(String name);
+    Product findById(long id);
+}
