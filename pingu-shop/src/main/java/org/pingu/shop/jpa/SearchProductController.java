c8cf677e39edd6a97b81f0d7e5265093b8424c61
MiguelNdeCarvalho <geral@miguelndecarvalho.pt>
2021-02-06 15:58:31 +0000

    Add SearchProductController.java

---
+++ pingu-shop/src/main/java/org/pingu/shop/jpa/SearchProductController.java
@@ -0,0 +1,28 @@
+package org.pingu.shop.jpa;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+public class SearchProductController {
+
+    @Autowired
+    private ProductRepository repository;
+    
+    @PostMapping("/search")
+	public String searchProduct(
+			@RequestParam(name="search", required=true, defaultValue="") String search,
+			Model model) 
+	{
+        List<Product> productList = (List<Product>) repository.findByNameContainingIgnoreCase(search);
+
+        model.addAttribute("productList", productList);
+		return "index";
+	}
+}
