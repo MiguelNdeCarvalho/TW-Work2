b125bac1351ccb4790ae6ff36214121749765efa
MiguelNdeCarvalho <geral@miguelndecarvalho.pt>
2021-02-06 16:13:52 +0000

    ProductRepository.java: change typo to list

--- pingu-shop/src/main/java/org/pingu/shop/jpa/ProductRepository.java
+++ pingu-shop/src/main/java/org/pingu/shop/jpa/ProductRepository.java
@@ -1,10 +1,10 @@
 package org.pingu.shop.jpa;
 
-
+import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ProductRepository extends CrudRepository<Product, Long> {
     Product findByName(String name);
     Product findById(long id);
-    Product findByNameContainingIgnoreCase(String name);
+    List<Product> findByNameContainingIgnoreCase(String name);
 }
