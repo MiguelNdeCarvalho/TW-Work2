6cfc6e411eed58d92a033a6b1a02c77b7eca4a90
MiguelNdeCarvalho <geral@miguelndecarvalho.pt>
2021-02-07 18:10:31 +0000

    Create AdministrationController.java

---
+++ pingu-shop/src/main/java/org/pingu/shop/jpa/AdministrationController.java
@@ -0,0 +1,38 @@
+package org.pingu.shop.jpa;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+public class AdministrationController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    // @Autowired
+    // private OrderRepository orderRepository;
+	
+	@GetMapping(value={"/admin"})
+	public String adminControl(Model model) 
+	{
+        List<User> userList = (List<User>) userRepository.findAll();
+		List<Product> productList = (List<Product>) productRepository.findAll(); //Get all products
+        // List<Orders> ordersList = (List<Orders>) repository.findAll(); //Get all orders
+
+        model.addAttribute("userList", userList);
+		model.addAttribute("productList", productList);
+        // model.addAttribute("orderList", orderList);
+        return "admin";
+	}
+}
