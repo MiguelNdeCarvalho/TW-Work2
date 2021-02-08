587273cf50c5881e02c8b08deb0f2ca7522761e1
MiguelNdeCarvalho <geral@miguelndecarvalho.pt>
2021-02-08 15:57:12 +0000

    OrdersRepository.java: Add findByUser method

--- pingu-shop/src/main/java/org/pingu/shop/jpa/OrdersRepository.java
+++ pingu-shop/src/main/java/org/pingu/shop/jpa/OrdersRepository.java
@@ -1,8 +1,9 @@
 package org.pingu.shop.jpa;
 
-
+import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 
 public interface OrdersRepository extends CrudRepository<Orders, Long> {
 	Orders findById(long id);
+	List<Orders> findByUser(User user);
 }
